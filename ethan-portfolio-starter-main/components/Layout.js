5e7b9d99cbf392e2c67c60be1b628ae7d9a544ef
Muhammadusman099 <usmandeveloper09@gmail.com>
2023-11-14 15:30:21 +0500

    main

--- ethan-portfolio-starter-main/components/Layout.js
+++ ethan-portfolio-starter-main/components/Layout.js
@@ -1,3 +1,15 @@
+// Fonts
+import {sora} from '@next/font/google'
+
+// Fonts setting
+const sora = sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
+
+
+
 const Layout = () => {
   return <div>Layout</div>;
 };
