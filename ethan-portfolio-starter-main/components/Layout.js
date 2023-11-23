cc52295898ee8719741a76198745508d09c8e33a
Muhammadusman099 <usmandeveloper09@gmail.com>
2023-11-23 17:31:55 +0500

    main

--- ethan-portfolio-starter-main/components/Layout.js
+++ ethan-portfolio-starter-main/components/Layout.js
@@ -17,7 +17,7 @@ import TopLeftImg from '../components/TopLeftImg'
 
 const Layout = ({ children }) => {
   return (
-    <div className='page bg-site text-white'>
+    <div className={'page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative'}>
       <TopLeftImg />
       <Nav />
       <Header />
