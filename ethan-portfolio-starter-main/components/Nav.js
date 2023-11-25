1b4cfe28acfaac899d62f5aae235e3257fe5dd81
Muhammadusman099 <usmandeveloper09@gmail.com>
2023-11-25 13:32:51 +0500

    main

--- ethan-portfolio-starter-main/components/Nav.js
+++ ethan-portfolio-starter-main/components/Nav.js
@@ -50,8 +50,10 @@ const Nav = () => {
             >
               {/* tooltip */}
               <div className='absolute pr-14  right-0 hidden xl:group-hover:flex'>
-                <div className='bg-white relative flex text-primary items-center p-[6px]'>
-                  <div className="text-[12px]">{link.name}</div>
+                <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                  <div className="text-[12px] leading-none font-semibold capitalize">{link.name}</div>
+                  {/*  triangle */}
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2 '></div>
                 </div>
               </div>
               {/* icon */}
--- ethan-portfolio-starter-main/pages/_app.js
+++ ethan-portfolio-starter-main/pages/_app.js
@@ -1,16 +1,26 @@
-import '../styles/globals.css';
-
+import "../styles/globals.css";
 
 // components
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 
+// router
+import { useRouter } from "next/router";
+// framer motion
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <div>
+    
+        </div>
+        <Component {...pageProps} />
+      </AnimatePresence>
     </Layout>
   );
-};
+}
 
 export default MyApp;
