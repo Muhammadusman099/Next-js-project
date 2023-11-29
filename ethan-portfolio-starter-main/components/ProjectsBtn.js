9af78a7316b5209f83c4aeffb68c8f29731b3a26
Muhammadusman099 <usmandeveloper09@gmail.com>
2023-11-29 14:11:23 +0500

    main

--- ethan-portfolio-starter-main/components/ProjectsBtn.js
+++ ethan-portfolio-starter-main/components/ProjectsBtn.js
@@ -1,18 +1,28 @@
-// next image 
+// next image
 import Image from "next/image";
 // next link
 import Link from "next/link";
 
 // icons
-import { HiArrowRight } from 'react-icons/hi2'
+import { HiArrowRight } from "react-icons/hi2";
 
 const ProjectsBtn = () => {
-  return  (
-  <div className='max-auto xl:mx-0 '>
-      <Link href={'/work'}>
-          <Image src={'/rounded-text.png'} width={141} height={148} alt='' className='animate-spin-slow w-full h-full max-w-[141px]' />
+  return (
+    <div className="max-auto xl:mx-0">
+      <Link
+        href={"/work"}
+        className="relative w-[160px] h-[160px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src={"/rounded-text.png"}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[120px]  max-h-[130px]"
+        />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300 " />
       </Link>
-  </div>
+    </div>
   );
 };
 
--- ethan-portfolio-starter-main/pages/index.js
+++ ethan-portfolio-starter-main/pages/index.js
@@ -1,36 +1,59 @@
 // next image
-import Image from 'next/image';
+import Image from "next/image";
 
 // componets
-import ParticlesContainer from '../components/ParticlesContainer';
-import ProjectsBtn from '../components/ProjectsBtn';
-import Avatar from '../components/Avatar';
-
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
 
 // framer motion
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 // variants
-import { fadeIn } from '../variants'
+import { fadeIn } from "../variants";
 
 const Home = () => {
-  return <div className='bg-primary/60 h-full'>
-    { /* text*/}
-    <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black-30'>
-      <div className=' flex flex-col justify-center  xl:pt-40 xl:text-left h-full container mx-auto'>
-        {/* title */}
-        <h1 className='h1'>Transforming Ideas <br /> Into <span className='text-accent'>Digital Reality</span></h1>
-        {/* subtitle */}
-        <p className='max-w-sm xl:max-w-xl max-auto xl:mx-0 mb-10 xl:mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maxime aut corrupti possimus reprehenderit numquam molestias corporis esse facere quo!</p>
-        {/* BTN */}
-        <div className=' xl:hidden'>
-          <ProjectsBtn />
+  return (
+    <div className="bg-primary/60 h-full ">
+      {/* text*/}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black-30 to-black/10">
+        <div className=" flex flex-col justify-center  xl:pt-20 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <h1 className="h1 sm-mt-20">
+            Transforming Ideas <br /> Into{""}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl max-auto xl:mx-0 mb-10"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            maxime aut corrupti possimus reprehenderit numquam molestias
+            corporis esse facere quo!
+          </motion.p>
+          {/* BTN */}
+          <div className=" xl:hidden">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
+      {/* Image*/}
+      <div>image</div>
     </div>
-    { /* Image*/}
-    <div>image</div>
-  </div>
-}
+  );
+};
 
-export default Home
+export default Home;
