a9f7be4696c20fddff80159c43a894f430766816
Muhammadusman099 <usmandeveloper09@gmail.com>
2023-11-29 17:05:09 +0500

    main

--- ethan-portfolio-starter-main/pages/index.js
+++ ethan-portfolio-starter-main/pages/index.js
@@ -19,10 +19,16 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black-30 to-black/10">
         <div className=" flex flex-col justify-center  xl:pt-20 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1 sm-mt-20">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1 sm-mt-20"
+          >
             Transforming Ideas <br /> Into{""}
             <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
           <motion.p
             variants={fadeIn("down", 0.3)}
@@ -51,7 +57,23 @@ const Home = () => {
         </div>
       </div>
       {/* Image*/}
-      <div>image</div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* particles */}
+        <ParticlesContainer />
+        {/* avatar img */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{duration:1, ease: 'easeInOut'}}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]  "
+        >
+          <Avatar />
+        </motion.div>
+      </div>
     </div>
   );
 };
